00a2057621b3177f71a8abf6d79f6996b101b379
Cypherman <tungnt.finance@gmail.com>
2020-02-28 17:59:07 +0700

    fix chrome plugin err

--- client/src/index.js
+++ client/src/index.js
@@ -13,14 +13,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const store = createStore(
-  reducers,
-  {},
-  compose(
-    applyMiddleware(reduxThunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(reducers, {}, compose(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
   <Provider store={store}>
